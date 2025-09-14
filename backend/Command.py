3948d6554eee513f912a0205b27b0e3c1ab32e15
codesR-cs <rajatavapal@gmail.com>
2025-09-14 17:00:27 +0530

    word changes

--- backend/Command.py
+++ backend/Command.py
@@ -1,6 +1,9 @@
 import pyttsx3
 def speak(text):
-    engine = pyttsx3.init()
+    engine = pyttsx3.init("sapi5")
+    voices=engine.getProperty('voices')
+    engine.setProperty('voice',voices[1].id)
+    engine.setProperty('rate',150)  
     engine.say(text)
     engine.runAndWait()
 speak("Hello, i am an ai assistance .")
